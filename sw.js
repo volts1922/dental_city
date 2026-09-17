e1268fd434debc96d118ab70a15a14068b335361
volts1922 <volts1922@yahoo.com>
2026-09-18 07:36:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VER = 'dental-city-clinic-v68.2'; // bumped: forces cached installs to fetch the fixed index.html
+const CACHE_VER = 'dental-city-clinic-v68.3'; // bumped: forces cached installs to fetch the fixed index.html
 const urlsToCache = [
   './',
   './index.html',
